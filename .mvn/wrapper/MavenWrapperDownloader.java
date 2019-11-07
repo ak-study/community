9c9896020a5334fcd03bbd6e444f66d0af1bb786
ak-study <1042491722@qq.com>
2019-11-07 11:45:11 +0800

    update

--- .mvn/wrapper/MavenWrapperDownloader.java
+++ .mvn/wrapper/MavenWrapperDownloader.java
@@ -38,6 +38,7 @@ public class MavenWrapperDownloader {
      * Path to the maven-wrapper.properties file, which might contain a downloadUrl property to
      * use instead of the default one.
      */
+    ///
     private static final String MAVEN_WRAPPER_PROPERTIES_PATH =
             ".mvn/wrapper/maven-wrapper.properties";
 
